12961208865c6f0dc4fc814eb4ad571af3a22581
Ben French <ben.jminw@gmail.com>
2018-02-09 01:20:30 -0700

    Placing order implemented.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,36 +13,89 @@ const sqlConnection = mysql.createConnection({
 
 const start = function() {
     // inquirer.prompt()
-    printItemList();
-    sqlConnection.end();
+
+    printItemList().then( () => {
+        inquirer.prompt(
+            [
+                {
+                    type: 'input',
+                    name: 'itemID',
+                    message: 'Please enter the ID of the item you would like to purchase.'
+                },
+                {
+                    type: 'input',
+                    name: 'itemQuantity',
+                    message: 'Please enter quantity you would like to purchase.'
+                }
+            ]
+        )
+        .then(answers => {
+            var itemID = answers.itemID;
+            var itemQuantity = answers.itemQuantity;
+
+            placeOrder(itemID, itemQuantity);
+            // sqlConnection.end();
+            // start();
+        });
+    })
+    
+
 }
 
 const printItemList = function() {
-    sqlConnection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        var data = [];
-        data.push(
-            [
-                chalk.underline.bold('ID'),
-                chalk.underline.bold('Name'),
-                chalk.underline.bold('Department Name'),
-                chalk.underline.bold('Price'),
-                chalk.underline.bold('Stock')
-            ]
-        );
-        res.forEach(element => {
+    return new Promise( (resolve, reject) => {
+        sqlConnection.query("SELECT * FROM products", function (err, res) {
+            if (err) reject(Error(err));
+            // Log all results of the SELECT statement
+            var data = [];
             data.push(
                 [
-                    element.item_id,
-                    element.product_name,
-                    element.department_name,
-                    element.price,
-                    element.stock_quantity
+                    chalk.underline.bold('ID'),
+                    chalk.underline.bold('Name'),
+                    chalk.underline.bold('Department Name'),
+                    chalk.underline.bold('Price'),
+                    chalk.underline.bold('Stock')
                 ]
             );
+            res.forEach(element => {
+                data.push(
+                    [
+                        element.item_id,
+                        element.product_name,
+                        element.department_name,
+                        element.price,
+                        element.stock_quantity
+                    ]
+                );
+            });
+            console.log(table(data));
+            resolve();
         });
-        console.log(table(data));
+    })
+}
+
+const placeOrder = function(itemId, itemQuantity) {
+    sqlConnection.query("SELECT price, stock_quantity FROM products WHERE item_id=?", [itemId], function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        if (res.length === 1) {
+            var total_price = res[0].price * itemQuantity;
+            if (itemQuantity <= res[0].stock_quantity) {
+                var newQuantity = res[0].stock_quantity - itemQuantity;
+                console.log('Placing Order...');
+                sqlConnection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [newQuantity, itemId], function (err, res) {
+                    if (err) throw err;
+                    console.log('Order Placed!');
+                    console.log("Cost of order = $%s", total_price.toFixed(2));
+                });
+            }
+            else {
+                console.log('Not enough in stock to place order');
+            }
+        }
+        else {
+            console.log('ItemID not found');
+        }
     });
 }
 
